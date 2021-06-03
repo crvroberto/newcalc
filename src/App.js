78de29283bfdd56bfd581a273c314b11ecdad25b
crvroberto <carlos010442@hotmail.com>
2021-06-03 15:08:21 -0300

    1 add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 import './App.css';
 
+
 function App() {
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
+
+
+  const classes = useStyles();
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form className={classes.root} noValidate autoComplete="off">
+
+        <TextField id="outlined-basic" 
+        label="Páginas" 
+        variant="outlined" 
+        type="number"
+        />
+      </form>
     </div>
   );
 }
