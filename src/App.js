3ebea0df05ce399a7e4c03d4dcee86ca773aa3c1
crvroberto <carlos010442@hotmail.com>
2021-06-03 16:40:36 -0300

    2 add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import './App.css';
@@ -6,34 +6,45 @@ import './App.css';
 
 function App() {
 
+  const [pag, setPag] = useState("")
+
   const useStyles = makeStyles((theme) => ({
     root: {
       '& > *': {
-        margin: theme.spacing(1),
+        margin: theme.spacing(30),
         width: '25ch',
       },
     },
-  }));
+  }))
+  const classes = useStyles()
 
+  
 
-  const classes = useStyles();
+  const produtos = {
 
-
+  }
 
 
   return (
     <div className="App">
 
-      <form className={classes.root} noValidate autoComplete="off">
+      <div className={classes.root} noValidate autoComplete="off">
 
-        <TextField id="outlined-basic" 
+        <TextField 
+        id="outlined-basic" 
         label="Páginas" 
         variant="outlined" 
         type="number"
+        inputRef={input => input && input.focus()}
+        value={pag}
+        onChange={i => {setPag(i.target.value)}
+      }
         />
-      </form>
+      </div>
+
+      
     </div>
-  );
+  )
 }
 
 export default App;
